ecf91243094a75c38f3d6befd0e9e7f26e69c4fa
hyogye <aster8826@naver.com>
2022-05-20 17:28:43 +0900

    oracle 관련 소스 수정

--- day07_dbtest/oracle_test.py
+++ day07_dbtest/oracle_test.py
@@ -3,31 +3,13 @@ from sklearn.model_selection import cross_val_score
 
 conn = cx_Oracle.connect('scott','tiger','localhost:1521/XE')
 cursor = conn.cursor()
-cursor.execute("select * from emp")
+cursor.execute("select * from emp where deptno = 10")
 
 # print(cursor)
 
-# for item in cursor:
-#     print(item[1],item[5])
+for item in cursor:
+    print(item[1],item[5])
 
-# conn.close()
+conn.close()
 
-# 1.emp 테이블에 job을 입력받아 해당 job인 사원을 출력하세요
-# 2.사원의 이름의 일부를 입력받아서 검색해서 출력
 
-# for item in cursor:
-#     input('[ename]을 입력하세요>>>')
-#     print(item)
-
-while True:
-    choice = input('''1.job 출력 2.사원 검색>>> ''')
-    if choice =='1': 
-        conn = cx_Oracle.connect('scott','tiger','localhost:1521/XE')
-        cursor = conn.cursor()
-        cursor.execute("select * from emp")
-            print('ename','job')
-        for item in cursor:
-            print(item[1],item[2])
-           
-    elif choice == '2':
-        pass
